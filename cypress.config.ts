dd230508c1297c72f1485b5ba2d829bb4e6895a4
Oladokun Oladipupo <115462144+oladokuno@users.noreply.github.com>
2023-03-15 12:47:36 +0100

    setup commit

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8888',
+    baseUrl: 'http://localhost:4200',
     supportFile: false,
   },
+  projectId: '74yrgi'
 });
