22aefe1cd8056689495d949bcb2b8d823edde218
Oladokun Oladipupo <115462144+oladokuno@users.noreply.github.com>
2023-04-23 13:30:54 +0100

    changes

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -4,9 +4,4 @@ describe('empty spec', () => {
   beforeEach(() => {
     cy.visit('/');
   });
-
-  it('displays the resources text', () => {
-    cy.get('p')
-      .contains('site works!');
-  });
 });
